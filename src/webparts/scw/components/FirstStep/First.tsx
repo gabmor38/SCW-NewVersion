dc7e3dad88238b0db264d92a7f290a7a1af11e95
gabmor38 <Gabriela.morenor@gmail.com>
2023-02-03 21:47:03 -0500

    added radio buttons

--- src/webparts/scw/components/FirstStep/First.tsx
+++ src/webparts/scw/components/FirstStep/First.tsx
@@ -1,13 +1,30 @@
-import * as React from 'react';
-import { FunctionComponent } from 'react';
-import { Stack } from 'office-ui-fabric-react';
+import * as React from "react";
+import { FunctionComponent } from "react";
+import { Stack } from "office-ui-fabric-react";
+import { IScwProps } from "../IScwProps";
 
-const First: FunctionComponent = () => {
+const First: FunctionComponent<IScwProps> = (props) => {
+
+  
   return (
     <Stack>
-      <div>1</div>
+      <main> 
+        <form>
+          <div>
+            <label>
+              <input type="radio" value="libraryTemplate" />
+              Library Template
+            </label>
+          </div>
+          <div>
+              <label>
+                <input type="radio" value="collaborationTemplate"/>
+               Collaboration Template
+             </label>
+            </div>
+        </form>
+      </main>
     </Stack>
-    
-  )
-}
+  );
+};
 export default First;
