4cf359daf350bac76b01b82bc7dd0bae360531c9
gabmor38 <Gabriela.morenor@gmail.com>
2023-02-04 23:57:46 -0500

    created third step form

--- src/webparts/scw/components/ThirdStep/Third.tsx
+++ src/webparts/scw/components/ThirdStep/Third.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
 import { FunctionComponent } from 'react';
+import styles from './Third.module.scss';
 
 const Third: FunctionComponent = () => {
   return (
-    <div>3</div>
+    <section className={styles.container}>
+    <p>A community description helps users to find your community with our search function. This 33 character description will be visible to users when they use our 
+      all communities page and when they search for it in the search bar.
+    </p>
+    <form>
+      <div className={styles.field} >
+        <label>Short English description</label>
+        <p>Maximum 33 characters in English.</p>
+        <input type="text" name="englishDescription" />
+      </div>
+      <div className={styles.field}>
+        <label>Short French description</label>
+        <p>Maximum 33 characters in French.</p>
+        <input type="text" name="frenchDescription" />
+      </div>
+      <div className={styles.field}>
+        <label>Business reason</label>
+        <p>In maximum 500 characters explain the purpose of this communityfor our support teamto determine whether or not it can be approved.
+          This will not show up on your site.
+        </p>
+        <input type="text" name="businessReason" />
+      </div>
+    </form>
+  </section>
   )
 }
 export default Third ;
